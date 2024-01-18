2d3adc4302fdfad24ef63447e20c009f9485adf0
laikahkeen <laikahkeen@gmail.com>
2024-01-18 08:26:17 +0800

    done

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,23 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // Required for expo-router
-      'expo-router/babel',
-    ],
-  };
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			// Required for expo-router
+			"expo-router/babel",
+			[
+				"module:react-native-dotenv",
+				{
+					envName: "APP_ENV",
+					moduleName: "@env",
+					path: ".env",
+					blocklist: null,
+					allowlist: null,
+					safe: false,
+					allowUndefined: true,
+					verbose: false,
+				},
+			],
+		],
+	};
 };
